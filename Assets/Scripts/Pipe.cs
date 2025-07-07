9b2d5603b8dbe076f3395f899476ab1ee652fdf0
AngelBratoev <angel.bratoev97@gmail.com>
2025-07-07 16:35:27 +0300

    Added logic manager and score counter
    Added logic manager object that keeps the game info and methods that update said info.
    
    Also added trigger collider to the pipe prefab, that increases the score every time the player goes through it

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -6,10 +6,13 @@ public class Pipe : MonoBehaviour
 {
     public GameObject pipe;
     public float movementSpeed;
+    public LogicScript logic;
+    public int scoreToAdd = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
@@ -22,4 +25,10 @@ public class Pipe : MonoBehaviour
             Destroy(pipe);
         }
     }
+
+    
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        logic.AddScore(scoreToAdd);
+	}
 }
